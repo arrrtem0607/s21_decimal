9a405c2eb2f98fcd259e8afff14d65fa050bcb21
artem <DlyaNas@MacBook-Air.local>
2024-01-12 19:05:58 +0300

    nice cmake

--- src/s21_convertors_and_parsers.c
+++ src/s21_convertors_and_parsers.c
@@ -12,32 +12,6 @@ int s21_from_int_to_decimal(s21_decimal *dst, int input) {
   return 0;
 }
 
-/*int s21_from_float_to_decimal(s21_decimal *dst, float input) {
-    int flag = 0;
-    s21_decl_to_null(dst);
-    if (fabs(input) < powl(10.0, -1 * MAX_POW)) {
-        flag = 1;
-        // dst->state = S21_NULL; хз нужно ли это, по идее программа завершится с ошибкой
-    } else if (input >= MAX_DECIMAL) {
-        flag = 1;
-        // dst->state = S21_PLUS_INF; хз нужно ли это, по идее программа завершится с ошибкой
-    } else if (input <= MIN_DECIMAL) {
-        flag = 1;
-        // dst->state = S21_MINUS_INF; хз нужно ли это, по идее программа завершится с ошибкой
-    } else {
-        int scale = 0;
-        if (input < 0)
-            s21_set_sign_31(1, dst);
-        input = fabsl(input);
-        while (!(int)input && scale < MAX_POW) {
-            input *= 10;
-            scale++;
-        }
-        s21_set_scale_ratio_16_23(scale, dst);
-    }
-  return flag;
-}
-*/
 
 int s21_from_float_to_decimal(s21_decimal *dst, float input) {
     if (input < 0){
