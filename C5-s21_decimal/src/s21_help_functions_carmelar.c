e5cbc9f32dcf46d462919e518230ef0ede68ad89
Artyom <beastdope@inbox.ru>
2024-01-13 17:30:56 +0300

    edit: error fix gcc

--- C5-s21_decimal/src/s21_help_functions_carmelar.c
+++ C5-s21_decimal/src/s21_help_functions_carmelar.c
@@ -1,4 +1,4 @@
-#include "s21_header.h"
+#include "s21_header_carmelar.h"
 
 /**
  * Функция s21_get_sign возвращает знак десятичного числа.
@@ -38,37 +38,52 @@ int s21_get_scale_ratio_16_23(s21_decimal number) {
 
 
 void s21_normalization(s21_decimal* number_1, s21_decimal* number_2) {
-  if (s21_get_scale_ratio_16_23(*number_1) !=
-      s21_get_scale_ratio_16_23(*number_2)) {
-    if (s21_get_scale_ratio_16_23(*number_1) <
-        s21_get_scale_ratio_16_23(*number_2)) {
-      s21_normalization(number_2, number_1);
+  int scale_1 = s21_get_scale_ratio_16_23(*number_1);
+  int scale_2 = s21_get_scale_ratio_16_23(*number_2);
+
+  if (scale_1 != scale_2) {
+    if (scale_1 < scale_2) {
+      s21_scale_increase(number_1, scale_2 - scale_1);
     } else {
-      int s21_scale_to_high = s21_get_scale_ratio_16_23(*number_2),
-          s21_scale_to_low = s21_get_scale_ratio_16_23(*number_1);
-      for (; (s21_scale_to_high - s21_scale_to_low) &&
-             !s21_get_bit(*number_2, 96);
-           s21_scale_to_low += 1)
-        s21_scale_increase(number_2, 1);
-      for (; s21_scale_to_high - s21_scale_to_low; s21_scale_to_high -= 1) {
-        s21_scale_decrease(number_1, s21_scale_to_high - s21_scale_to_low);
-        s21_set_scale_ratio_16_23(number_1, s21_scale_to_low);
-      }
+      s21_scale_increase(number_2, scale_1 - scale_2);
     }
   }
-}  //приводит к общему знаменателю
-
-int s21_scale_increase(s21_decimal* number, int shift) {
-  if (s21_get_scale_ratio_16_23(*number) + shift < 29) {
-    s21_set_scale_ratio_16_23(s21_get_scale_ratio_16_23(*number) + shift, number);
-    s21_decimal additional_number_1 = *number, additional_number_2 = *number;
-    if (s21_add_bit(*s21_shift_to_left(&additional_number_1, 1), 
-    *s21_shift_to_left(&additional_number_2, 3), number))
-      s21_set_bit(number, 96, 1);
-  }
-  return number;
 }
+ //приводит к общему знаменателю
+/**
+ * Функция увеличивает масштаб десятичного числа на указанную величину сдвига.
+ * 
+ * @param number Параметр «число» является указателем на переменную типа «s21_decimal».
+ * @param shift Параметр «сдвиг» в данном коде представляет собой величину, на которую необходимо
+ * увеличить масштаб десятичного числа.
+ * 
+ * @return указатель на число s21_decimal.
+ */
 
+int s21_scale_increase(s21_decimal* number, int shift) { 
+  if (s21_get_scale_ratio_16_23(*number) + shift < 29) 
+  { s21_set_scale_ratio_16_23(number, s21_get_scale_ratio_16_23(*number) + shift);
+
+s21_decimal additional_number_1 = *s21_shift_to_left(number, 1);
+s21_decimal additional_number_2 = *s21_shift_to_left(number, 3);
+
+if (s21_add_bit(additional_number_1, additional_number_2, number))
+  s21_set_bit(96, 1, number);
+  }
+
+return 1; 
+} 
+
+/**
+ * Функция `s21_scale_decrease` уменьшает масштаб десятичного числа, сдвигая его цифры вправо.
+ * 
+ * @param number Указатель на структуру s21_decimal, которая представляет десятичное число в
+ * пользовательском формате с фиксированной запятой.
+ * @param shift Параметр «shift» в функции «s21_scale_decrease» представляет количество десятичных
+ * знаков для уменьшения масштаба данного «числа».
+ * 
+ * @return указатель на структуру s21_decimal.
+ */
 s21_decimal* s21_scale_decrease(s21_decimal* number, int shift) {
   for (int y = 0; y < shift; y += 1) {
     unsigned long long overflow = number->bits[2];
@@ -86,16 +101,17 @@ s21_decimal* s21_scale_decrease(s21_decimal* number, int shift) {
 void print_s21_decimal(s21_decimal num) {
     printf("Value: %08x %08x %08x %08x\n", num.bits[0], num.bits[1], num.bits[2], num.bits[3]);
 }
+
+
 int main() {
     // Создайте два числа s21_decimal для тестирования
-    s21_decimal num1 = {{0x12345678, 0x9ABCDEF0, 0xABCDEF01, 0x23456789}};
-    s21_decimal num2 = {{0xFEDCBA98, 0x76543210, 0x98765432, 0xABCDEF01}};
+    s21_decimal num1 = {{01234564.412300}};
+    s21_decimal num2 = {{123454.123455356000}};
     
     printf("Values before:\n");
     print_s21_decimal(num1);
     print_s21_decimal(num2);
 
-    // Вызовите функцию нормализации
     s21_normalization(&num1, &num2);
 
     printf("Values after:\n");
@@ -107,6 +123,21 @@ int main() {
 
 
 
+/**
+ * Функция `s21_add_bit` складывает два числа s21_decimal и сохраняет результат в третьем s21_decimal,
+ * а также обрабатывает условия переноса и переполнения.
+ * 
+ * @param number_1 Параметр «number_1» имеет тип «s21_decimal», который представляет собой
+ * пользовательский тип данных, представляющий десятичное число в определенном формате. Он используется
+ * для хранения первого десятичного числа для сложения.
+ * @param number_2 Параметр «number_2» — это переменная типа «s21_decimal», которая представляет
+ * десятичное число в определенном формате. Он используется как один из операндов в операции сложения.
+ * @param result Параметр result является указателем на переменную s21_decimal. Эта переменная будет
+ * хранить результат сложения `number_1` и `number_2`.
+ * 
+ * @return целочисленное значение. Если условие в операторе if в строке 26 истинно, функция возвращает
+ * 1. В противном случае она возвращает 0.
+ */
 int s21_add_bit(s21_decimal number_1, s21_decimal number_2,
                 s21_decimal* result) {
   int carry = 0;
@@ -126,7 +157,7 @@ int s21_add_bit(s21_decimal number_1, s21_decimal number_2,
       sum = carry++;
     }
 
-    s21_set_bit(result, i, sum);
+    s21_set_bit(i, sum, result);
 
     if (i == 95 && carry == 1 && s21_get_bit(number_1, 97) == 0 &&
         s21_get_bit(number_2, 97) == 0) {
@@ -143,8 +174,8 @@ s21_decimal* s21_shift_to_left(s21_decimal* number, int shift) {
       int bit_to_transfer_1 = s21_get_bit(*number, 31);
       int bit_to_transfer_2 = s21_get_bit(*number, 63);
       for (int x = 0; x < 3; x += 1) number->bits[x] <<= 1;
-      if (bit_to_transfer_1) s21_set_bit(number, 32, 1);
-      if (bit_to_transfer_2) s21_set_bit(number, 64, 1);
+      if (bit_to_transfer_1) s21_set_bit(32, 1, number);
+      if (bit_to_transfer_2) s21_set_bit(64, 1, number);
     }
   }
   return number;
@@ -233,7 +264,7 @@ int s21_sub_mantissas(s21_decimal one, s21_decimal two, s21_decimal* result) {
       remember = 0;
     }
 
-    s21_set_bit(i, res_value, &temp);
+    s21_set_bit(i, res_value, (s21_decimal*)&temp);
   }
 
   flag = s21_copy_bigdecimal_to_decimal(temp, result);
