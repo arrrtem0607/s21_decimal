7b04e88bd5adb1aa215599109d7b5730ba493ca9
naillora <naillora@student.21-school.ru>
2024-01-15 21:05:12 +0300

    вроде заработало умножениеэ

--- src/s21_arithmetic_operators.c
+++ src/s21_arithmetic_operators.c
@@ -66,20 +66,28 @@ int s21_mul(s21_decimal value_1, s21_decimal value_2, s21_decimal *result){
         s21_set_sign_31(1, result);
     }
     s21_set_scale_ratio_16_23(s21_get_scale_ratio_16_23(value_1) + s21_get_scale_ratio_16_23(value_2), result);
+    int bit_31 = 0;
+    int bit_63 = 0;
+    int bit_95 = 0;
     for (int i = 0; i < START_INFO; i++){
+        bit_31 = s21_get_bit(value_1, 31);
+        bit_63 = s21_get_bit(value_1, 63);
+        bit_95 = s21_get_bit(value_1, 95);
         result -> bits[0] += value_1.bits[0] * s21_get_bit(value_2, i);
         result -> bits[1] += value_1.bits[1] * s21_get_bit(value_2, i);
         result -> bits[2] += value_1.bits[2] * s21_get_bit(value_2, i);
         value_1.bits[0] <<= 1;
         value_1.bits[1] <<= 1;
         value_1.bits[2] <<= 1;
-        if (s21_get_bit(value_2, 31)) {
-            value_1.bits[1] |= 1;
-        } else if (s21_get_bit(value_2, 63)) {
-            value_1.bits[2] |= 1;
-
+        if (bit_31) {
+            s21_set_bit(32, 1, &value_1);
         }
-
+        if (bit_63) {
+            s21_set_bit(64, 1, &value_1);
+        }
+        if (bit_95) {
+            s21_set_bit(96, 1, &value_1);
+        }
     }
     return 0;
 }
