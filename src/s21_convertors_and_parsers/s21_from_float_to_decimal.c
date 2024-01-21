c963517775b0c85dffedfea7fa7ce23244398a43
naillora <naillora@student.21-school.ru>
2024-01-21 19:10:01 +0300

    гружу на гитлаб

--- src/s21_convertors_and_parsers/s21_from_float_to_decimal.c
+++ src/s21_convertors_and_parsers/s21_from_float_to_decimal.c
@@ -1,6 +1,9 @@
 #include "../s21_decimal.h"
 
 int s21_from_float_to_decimal(s21_decimal *dst, float input) {
+    s21_decl_to_null(dst);
+    if (isnan(input) || isinf(input) || fabs(input) > MAX_DECIMAL)
+        return 1;
     if (input < 0){
         s21_set_sign_31(1 ,dst);
         input *= -1;
